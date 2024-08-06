abd86fe562007bf657f2991feba6fc2fa9acc527
Kadiri Lavanya <kadirilavanya07@gmail.com>
2024-08-06 08:34:13 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,61 @@
+# build a command line calculator application using python and handle user input validation to ensure the calculator behaves predictably
+
+def add(x, y):
+  """Adds two numbers."""
+  return x + y
+
+def subtract(x, y):
+  """Subtracts two numbers."""
+  return x - y
+
+def multiply(x, y):
+  """Multiplies two numbers."""
+  return x * y
+
+def divide(x, y):
+  """Divides two numbers."""
+  if y == 0:
+    return "Division by zero!"
+  else:
+    return x / y
+
+while True:
+  print("Select operation:")
+  print("1. Add")
+  print("2. Subtract")
+  print("3. Multiply")
+  print("4. Divide")
+  print("5. Exit")
+
+  choice = input("Enter choice(1/2/3/4/5): ")
+
+  if choice in ('1', '2', '3', '4'):
+    try:
+      num1 = float(input("Enter first number: "))
+      num2 = float(input("Enter second number: "))
+    except ValueError:
+      print("Invalid input. Please enter numbers only.")
+      continue
+
+    if choice == '1':
+      print(num1, "+", num2, "=", add(num1, num2))
+      print()
+
+    elif choice == '2':
+      print(num1, "-", num2, "=", subtract(num1, num2))
+      print()
+
+    elif choice == '3':
+      print(num1, "*", num2, "=", multiply(num1, num2))
+      print()
+
+    elif choice == '4':
+      print(num1, "/", num2, "=", divide(num1, num2))
+      print()
+
+  elif choice == '5':
+    break
+
+  else:
+    print("Invalid Input")
+    print()
